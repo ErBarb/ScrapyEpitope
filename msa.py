972fe75c393941d19ecf80fd60ea16cc9a1ecfe8
ErBarb <erald.bb@gmail.com>
2022-04-19 17:56:08 +0200

    Reformated function get_conserved_sequences, added other arguments, added docstring

--- msa.py
+++ msa.py
@@ -1,7 +1,6 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import re
@@ -32,18 +31,48 @@ def alignment(seq_list):
 path_to_muscle = 'C:/Users/barbu/PycharmProjects/pythonProject/Pipeline/msa_results/muscle/muscle_spike.aln-fasta.fasta'
 path_to_mafft = 'C:/Users/barbu/PycharmProjects/pythonProject/Pipeline/msa_results/mafft/mafft_spike.aln-fasta.fasta'
 
-def get_conserved_sequences(path_to_file):
+def get_conserved_sequences(path_to_file, min_seq_conserved_pos = None, min_seq_flank_pos = None, max_contigous_nonconserved_pos = None, min_length_block = None, allowed_gap_pos = None):
+
+    """This function uploads the alignment file and sends the parameters to the Gblocks NGPhylogeny website,
+    then returns a dictionary with the conserved regions of each protein """
+
     gblocks_url = 'https://ngphylogeny.fr/tools/tool/276/form'
     driver = webdriver.Chrome(executable_path="chromedriver")
     driver.maximize_window()
     driver.get(gblocks_url)
 
-    driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/form/fieldset/div/div/div[1]/div/input").send_keys(path_to_file)
-    driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/form/input").click()
+    upload_file = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/form/fieldset/div/div/div[1]/div/input")
+    upload_file.send_keys(path_to_file)
+
+    data_type = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/form/fieldset/div/div/div[2]/div[1]/div/select")
+    data_type.send_keys('Protein')
+
+    b1 = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/form/fieldset/div/div/div[3]/div/input")
+    b1.clear()
+    b1.send_keys(min_seq_conserved_pos)
+
+    b2 = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/form/fieldset/div/div/div[4]/div/input")
+    b2.clear()
+    b2.send_keys(min_seq_flank_pos)
+
+    b3 = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/form/fieldset/div/div/div[5]/div/input")
+    b3.clear()
+    b3.send_keys(max_contigous_nonconserved_pos)
+
+    b4 = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/form/fieldset/div/div/div[6]/div/input")
+    b4.clear()
+    b4.send_keys(min_length_block)
+
+    b5 = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/form/fieldset/div/div/div[7]/div/select")
+    b5.send_keys(allowed_gap_pos)
+
+    submit = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/form/input")
+    submit.click()
     wait = WebDriverWait(driver, 180)
 
     wait.until(ec.visibility_of_element_located((By.XPATH, "/html/body/div/div[1]/div/div/div[6]/div/table/tbody/tr[2]/td[5]/a[5]")))
-    driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div/div[6]/div/table/tbody/tr[2]/td[5]/a[5]").click()
+    sequence_info_html = driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div/div[6]/div/table/tbody/tr[2]/td[5]/a[5]")
+    sequence_info_html.click()
 
     wait.until(ec.visibility_of_element_located(
         (By.XPATH, "/html/body/div/div[1]/div[2]/pre/pre[3]/b[1]")))
@@ -54,7 +83,8 @@ def get_conserved_sequences(path_to_file):
     driver.execute_script("window.history.go(-1)")
     wait.until(ec.visibility_of_element_located(
         (By.XPATH, "/html/body/div/div[1]/div/div/div[6]/div/table/tbody/tr[3]/td[4]/a[5]")))
-    driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div/div[6]/div/table/tbody/tr[3]/td[4]/a[5]").click()
+    cleaned_sequences_fasta = driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div/div[6]/div/table/tbody/tr[3]/td[4]/a[5]")
+    cleaned_sequences_fasta.click()
 
     wait.until(ec.visibility_of_element_located(
         (By.XPATH, "/html/body/div/div[1]/div[2]/pre")))
@@ -75,13 +105,12 @@ def get_conserved_sequences(path_to_file):
             cleaned_fasta_all = cleaned_fasta_all[cut_point:]
         conserved_sequences_dictionary[protein_id] = list_of_fasta
 
-    for i in conserved_sequences_dictionary:
-        print(i, conserved_sequences_dictionary[i])
-
-    #time.sleep(60)
     driver.close()
+    return conserved_sequences_dictionary
 
 
-get_conserved_sequences(path_to_muscle)
-get_conserved_sequences(path_to_mafft)
+#get_conserved_sequences(path_to_muscle)
+seq_dict = get_conserved_sequences(path_to_mafft, min_seq_conserved_pos='default', min_seq_flank_pos='default', max_contigous_nonconserved_pos = 8, min_length_block= 10, allowed_gap_pos='None')
+for i in seq_dict:
+    print(i, seq_dict[i])
 
